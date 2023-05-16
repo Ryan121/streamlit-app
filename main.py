01e9e5020fea6607377a9c28b174e472f256a333
Sers <Ryan.Sers@roke.co.uk>
2023-05-16 17:59:48 +0100

    Additional content added

--- main.py
+++ main.py
@@ -2,12 +2,140 @@ import streamlit as st
 from streamlit_echarts import st_echarts
 import pandas as pd
 import matplotlib.pyplot as plt
+from streamlit_lottie import st_lottie
+import requests
+from PIL import Image
+
+
+def load_lottieurl(url):
+    request = requests.get(url)
+
+    if request.status_code != 200:
+        return None
+    else:
+        return request.json()
 
 # Set webpage icon and tab title
 # For additional page icons go to: webfx.com/tools/emoji-cheat-sheet
 st.set_page_config(page_title="Example App",
                    page_icon=":mechanical_arm:",
                    layout="wide")
+
+
+img_meme_man = Image.open("images/meme_man.png")
+
+
+def home():
+
+    with st.container():
+        st.subheader("Hi, this site is a test project :collision:")
+        st.title("It tests Streamlit - An easy to use framework")
+        st.subheader("What a great introduction that was :zzz:")
+
+    with st.container():
+        st.write("---")
+        
+        left_col, right_col = st.columns(2)
+        left_col.error('Background')
+        right_col.success('Project')
+
+        with left_col:
+
+            st.write('##')
+
+            st.write("""
+
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Sollicitudin tempor id eu nisl. Aliquam vestibulum morbi blandit cursus. Egestas purus viverra accumsan in nisl nisi scelerisque eu. 
+                Nec ultrices dui sapien eget mi proin sed libero. Nulla pharetra diam sit amet nisl suscipit adipiscing. Vestibulum rhoncus est pellentesque 
+                elit ullamcorper dignissim cras tincidunt lobortis. Risus pretium quam vulputate dignissim suspendisse in. Pellentesque diam volutpat commodo 
+                sed. Amet aliquam id diam maecenas ultricies mi eget. Nisi lacus sed viverra tellus. Pharetra massa massa ultricies mi quis. Quam elementum pulvinar 
+                etiam non quam. Mi proin sed libero enim sed faucibus. Adipiscing bibendum est ultricies integer quis auctor elit. Id nibh tortor id aliquet lectus 
+                proin nibh nisl.
+
+                Nulla at volutpat diam ut venenatis. Aliquet risus feugiat in ante metus dictum at. Cursus metus aliquam eleifend mi in. Cursus in hac habitasse platea. 
+                Sit amet mattis vulputate enim nulla aliquet. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis nisl. Nibh venenatis cras sed felis. Id 
+                consectetur purus ut faucibus pulvinar elementum integer. Ut venenatis tellus in metus vulputate eu scelerisque. Amet purus gravida quis blandit turpis cursus. 
+                Id donec ultrices tincidunt arcu non sodales neque. Arcu ac tortor dignissim convallis aenean et tortor at. Porttitor rhoncus dolor purus non enim praesent 
+                elementum facilisis leo. Tortor id aliquet lectus proin nibh nisl condimentum id venenatis. Risus ultricies tristique nulla aliquet enim.
+
+                - Ut venenatis tellus in metus vulputate eu scelerisque.
+                - Tortor id aliquet lectus proin nibh nisl condimentum id venenatis.
+                - Potato por favor purus ut faucibus pulvinar elementum integer.
+            
+            """)
+
+            st.write("[Streamlit website link > ](https://streamlit.io/)")
+
+    # Website animation
+    # https://lottiefiles.com/
+
+    animation = "https://assets10.lottiefiles.com/packages/lf20_cHA3rG.json"
+
+    lottie_anim = load_lottieurl(animation)
+
+    with right_col:
+        st_lottie(lottie_anim, height=400, key='image')
+
+        st.write("""
+
+            - Ut venenatis tellus in metus vulputate eu scelerisque.
+            - Tortor id aliquet lectus proin nibh nisl condimentum id venenatis.
+            - Potato por favor purus ut faucibus pulvinar elementum integer.
+        
+        """)
+
+
+    with st.container():
+        st.write("---")
+        st.header("Projects")
+        st.write('##')
+
+        image_col, text_col = st.columns((1, 2))
+
+        with image_col:
+            st.image(img_meme_man)
+
+        with text_col:
+            st.subheader("How to work with streamlit")
+
+            st.write("""
+
+                - Ut venenatis tellus in metus vulputate eu scelerisque.
+                - Tortor id aliquet lectus proin nibh nisl condimentum id venenatis.
+                - Potato por favor purus ut faucibus pulvinar elementum integer.
+
+            """)
+
+    st.markdown("[Watch video...](https://www.youtube.com/watch?v=JwSS70SZdyM&t=288s)")
+
+    st.write('---')
+    st.write('##')
+
+    st.subheader('Resources')
+
+    st.write('##')  
+
+    col1, col2, col3 = st.columns(3)
+
+    # Image with embedded link
+    with col1:
+        # st.markdown("[![Foo](https://img.icons8.com/material-outlined/48/000000/github.png)](https://www.youtube.com/watch?v=SU--XMhbWoY)")
+        st.video('https://www.youtube.com/watch?v=SU--XMhbWoY')
+    with col2:
+        st.video('https://www.youtube.com/watch?v=VpAH2IoMzKw&t=777s')
+    with col3:
+        st.video('https://www.youtube.com/watch?v=XaFH2PcYI64')
+    
+
+    ex1, ex2 = st.columns(2)
+    ex1.error('GitHub Source')
+    ex2.success('Project')
+
+    # Site footer
+    # with container:
+    #     st.title('Application footer')
+
 
 # Define sections
 container = st.container()
@@ -17,7 +145,7 @@ def extractdata():
 
     df = pd.read_csv("stroke-data-main.csv")
     # print(dataset)
-    st.header('Raw Dataframe')
+    st.header('Step 1: Data Exploration')
 
     with open('styles.css') as cs:
         st.markdown(f'<style> { cs.read() } </style>', unsafe_allow_html=True)
@@ -72,48 +200,68 @@ def extractdata():
         "stroke == @stroke & smoking_status == @smoking_status & Residence_type == @Residence_type & work_type == @work_type & ever_married == @ever_married & heart_disease == @heart_disease & hypertension == @hypertension & gender == @gender"
     )
 
+    st.write('##')
+
     # Return filtered dataframe
     st.dataframe(df_selection)
 
-    st.title('Data Exploration')
+    st.write('---')
 
-    st.header('Duplicate rows')
+    st.header('Step 2: Data Cleaning')
+
+    st.subheader('Duplicate rows')
     dup_rows_df = df[df.duplicated()]
-    st.subheader(dup_rows_df)
+    st.write(f'{sum(dup_rows_df.count())}, duplicate rows found within the data')
 
-    st.header('Null Values')
+    st.subheader('Null or NaN Values')
     null_values = df.isnull().sum()
-    st.subheader(null_values)
+    st.write(f"{null_values.count()} NaN's or nullable rows found with the data")
     
     col1, col2 = st.columns(2)
+    
+    # Percentage of mising BMI and Smoking values
+    SmSt_mv = (df['smoking_status'].isnull().sum() / len(df['smoking_status'])*100)
 
+    # Percentage of missing values
+    BMI_mv = (df['bmi'].isnull().sum() / len(df['bmi'])*100)
+    
     with col1:
-        # Percentage of mising BMI and Smoking values
-        SmSt_mv = (df['smoking_status'].isnull().sum() / len(df['smoking_status'])*100)
-        st.header('Smoking Status missing values:')
-        st.subheader(f'{round(SmSt_mv, 1)}%')
-
+        st.subheader('Smoking Status missing values:')      
+    col1.error( f'{round(SmSt_mv, 1)}%', icon="🚨" ) 
     
     with col2:
-        # Percentage of missing values
-        BMI_mv = (df['bmi'].isnull().sum() / len(df['bmi'])*100)
-        st.header('BMI missing values:')
-        st.subheader(f'{round(BMI_mv, 1)}%')
+        st.subheader('BMI missing values:')
+    col2.warning( f'     {round(BMI_mv, 1)}%', icon="⚠️" )
+
+    st.write('##')
+    st.subheader('Histogram plot of BMI')
+    col1,col2,col3 = st.columns((1,2,1))
+    with col2:
+        fig = plt.figure(figsize=(5,5)) # try different values
+        ax = plt.axes()
+        ax.hist(df['bmi'], bins = 20, color = "c", edgecolor='black')
+        st.pyplot(fig)
+
+    st.write('##')
+    st.write("""
+        Analysis Decisions:
+
+        - Remove null/NaN rows
+        - Remove 'Smoking Status' column all together due to the high prevelence of missing values in reference to the the total number
+        - Impute BMI missing values. Use the median of the data due to the non-parametric distribution. 
+    
+    """)
+
+    st.write('##')
 
     # Drop smoking status & impute missing BMI values (too many missing values to impute smoking status)
     new_df = df.drop(['smoking_status'], axis=1)
 
-    st.header('New Dataframe after removing smoking status data')
-    st.subheader('This category was removed due to too many missing values')
+    st.header('New Dataframe after removing null values and smoking status data')
     st.dataframe(new_df)
 
     st.header('Key Stats')
     st.write(new_df.describe())
-    
-    fig = plt.figure(figsize=(5,5)) # try different values
-    ax = plt.axes()
-    ax.hist(df['bmi'], bins = 20, color = "c", edgecolor='black')
-    st.pyplot(fig)
     
 
     # Impute missing BMI level rows with BMI median
@@ -175,22 +323,7 @@ def extractdata():
 def homepage():
     # Site header
     with container:
-        st.title('Welcome to this dev streamlit app')
-        st.subheader('Contents')
-
-        # Image with embedded link
-        st.markdown("[![Foo](https://img.icons8.com/material-outlined/48/000000/github.png)](https://www.youtube.com/watch?v=SU--XMhbWoY)")
-
-        # Video
-        st.video('https://www.youtube.com/watch?v=SU--XMhbWoY')
-
-        ex1, ex2 = st.columns(2)
-        ex1.error('GitHub Source')
-        ex2.success('Project')
-
-    # Site footer
-    with container:
-        st.title('Application footer')
+        home()
 
 
 def projectpage():
@@ -251,7 +384,7 @@ def projectpage():
 
 def navigation():
 
-    current_page = st.sidebar.selectbox('Select a page', ('Home', 'Project #1', 'Project #2', 'Project #3'))
+    current_page = st.sidebar.selectbox('Select a page', ('Home', 'Project #1'))
     
     if current_page == 'Home':
         homepage()
